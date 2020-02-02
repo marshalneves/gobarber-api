d5256182617a46bb6b6ec1c98073d5a9cd5b9f85
Marshal Neves <mcneves75@gmail.com>
2020-02-02 13:43:46 -0300

    Cancelamento e envio de emails: Configurando nodemailer e enviando email de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f058a0a56a3e0c',
+    pass: 'ec761bf7c86c94',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
