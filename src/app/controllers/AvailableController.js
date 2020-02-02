873172e15ad9e31fede3429e085113141259f3e9
Marshal Neves <mcneves75@gmail.com>
2020-02-02 16:16:12 -0300

    Cancelamento e envio de emails: Tratamento de exceções com Sentry e Youch

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -21,7 +21,7 @@ class AvailableController {
     const searchDate = Number(date);
 
     // Obtém todos os agendamentos na data informada
-    const appointments = await Appointment.findAll({
+    const appointments = await Appointment.finddAll({
       where: {
         provider_id: req.params.providerId,
         canceled_at: null,
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://8bd70fbe2e614945b06030b9db31fce6@sentry.io/2187423',
+};
